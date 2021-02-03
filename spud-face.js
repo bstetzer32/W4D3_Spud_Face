c91613a6f30f3ecf8fffaae50542702c9d4c81fc
schuler <schuler.small.10@gmail.com>
2021-02-03 15:24:19 -0600

    completed phases 1 & 2

--- spud-face.js
+++ spud-face.js
@@ -1,4 +1,25 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  let addFormElement = function(event) {
+
+      let infoId = "card-" + event.target.id;
+      let info = document.getElementById(infoId);
+
+      if (infoId === "card-license-num-confirm") {
+      }  
+
+      else if (infoId === "card-donor-status") {
+        if (event.target.checked) {
+          info.innerText = "Organ Donor";
+        } else {
+          info.innerText = "";
+        } 
+      } else {
+          info.innerText = event.target.value;
+      } 
+  }
+
+  let form = document.getElementById("drivers-license-form");
+  form.addEventListener("input", addFormElement);
 
   // ** Phase 1B: Update license with event delegation and event.target ** 
 
