cc200215256830f4ad415c083fd3afdea317e2f2
Ben Stetzer <benstetzer@gmail.com>
2021-02-03 17:23:25 -0500

    phase 3 complete

--- spud-face.js
+++ spud-face.js
@@ -1,35 +1,52 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  let addFormElement = function(event) {
-
-      let infoId = "card-" + event.target.id;
-      let info = document.getElementById(infoId);
-
-      if (infoId === "card-license-num-confirm") {
-      }  
-
-      else if (infoId === "card-donor-status") {
-        if (event.target.checked) {
-          info.innerText = "Organ Donor";
-        } else {
-          info.innerText = "";
-        } 
+  // ** Phase 1B: Update license with event delegation and event.target ** 
+  let addFormElement = function (event) {
+    
+    let infoId = "card-" + event.target.id;
+    let info = document.getElementById(infoId);
+    
+    if (infoId === "card-license-num-confirm") {
+      let licenseNum = document.getElementById("license-num")
+      if (event.target.value !== licenseNum.value) {
+        event.target.style.backgroundColor = "lightcoral";
+        licenseNum.style.backgroundColor = "lightcoral";
       } else {
-          info.innerText = event.target.value;
-      } 
+        event.target.style.backgroundColor = "lightgreen";
+        licenseNum.style.backgroundColor = "initial";
+      }
+    }
+    
+    else if (infoId === "card-donor-status") {
+      if (event.target.checked) {
+        info.innerText = "❤️ Organ Donor ❤️";
+      } else {
+        info.innerText = "";
+      }
+    } else {
+      info.innerText = event.target.value;
+    }
   }
-
   let form = document.getElementById("drivers-license-form");
   form.addEventListener("input", addFormElement);
-
-  // ** Phase 1B: Update license with event delegation and event.target ** 
-
-
+  
   // ** Phase 2: Add focus and blur events to form inputs **
- 
-
+  let form2 = document.getElementsByClassName("form__input")
+  let focus = function (event) {
+    event.target.style.backgroundColor = "lightgreen";
+    
+  }
+  let blur = function (event) {
+    event.target.style.backgroundColor = "initial";
+    
+  }
+  Array.from(form2).forEach((input) => {
+    input.addEventListener("focus", focus)
+    input.addEventListener("blur", blur)
+  })
+  
   // ** Phase 3: Check that license numbers match **
- 
-
+  //              See lines 8- 17
+  
   // ** Phase 4: Update submit button click count **
 
  
